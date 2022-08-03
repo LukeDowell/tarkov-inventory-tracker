dec045d2f8bb00ce650b6033df7ae4e8d0dd007f
Luke Dowell <lukedowell@gmail.com>
2022-08-02 20:28:12 -0500

    Oops moved some stuff around

---
+++ tarkov_inventory_tracker/app.py
@@ -0,0 +1,30 @@
+import time
+
+import cv2
+import mss
+import numpy
+
+
+with mss.mss() as sct:
+    # Part of the screen to capture
+    monitor = {"top": 0, "left": 0, "width": 1000, "height": 1000}
+
+    while "Screen capturing":
+        last_time = time.time()
+
+        # Get raw pixels from the screen, save it to a Numpy array
+        img = numpy.array(sct.grab(monitor))
+
+        # Display the picture
+        cv2.imshow("OpenCV/Numpy normal", img)
+
+        # Display the picture in grayscale
+        # cv2.imshow('OpenCV/Numpy grayscale',
+        #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
+
+        print("fps: {}".format(1 / (time.time() - last_time)))
+
+        # Press "q" to quit
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            break
