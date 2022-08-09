edbf49cad90fff592bc8f3fe89c3a3818eab9c06
Luke Dowell <lukedowell@gmail.com>
2022-08-08 20:10:10 -0500

    Generating templates from items.json

--- tracker/generatetemplate.py
+++ tracker/generatetemplate.py
@@ -1,56 +1,46 @@
+import json
 from pathlib import Path
 
 from PIL import Image, ImageDraw, ImageFont
 
+script_path = Path(__file__).parent
+font_bold_path = (script_path / '../data/Jovanny Lemonad - Bender-Bold.otf').resolve()
+item_json_path = (script_path / '../data/items.json').resolve()
+item_font = ImageFont.truetype(font_bold_path.as_posix(), 17)
+ui_font = ImageFont.truetype(font_bold_path.as_posix(), 20)
 
-def to_item_template(n: str, font: ImageFont) -> (str, Image):
-    temp_w = int(font.getlength(n)) + 2
+
+def to_item_template(n: str) -> Image:
+    temp_w = int(item_font.getlength(n)) + 2
     temp_img = Image.new('RGBA', (temp_w, 17), (0, 0, 0, 0))
-    ImageDraw.Draw(temp_img).text((1, -1), n, fill=(211, 220, 220), font=font, stroke_width=1, stroke_fill=(0, 0, 0))
-    return n, temp_img
+    ImageDraw.Draw(temp_img).text((1, -1), n, fill=(211, 220, 220), font=item_font, stroke_width=1,
+                                  stroke_fill=(0, 0, 0))
+    return temp_img
 
 
-def to_ui_template(n: str, font: ImageFont) -> (str, Image):
-    temp_w = int(font.getlength(n)) + 2
+def to_ui_template(n: str) -> Image:
+    temp_w = int(ui_font.getlength(n)) + 2
     temp_img = Image.new('RGB', (temp_w, 20), (40, 40, 40))
-    ImageDraw.Draw(temp_img).text((1, -1), n, fill=(211, 220, 220), font=font, stroke_fill=(0, 0, 0))
-    return n, temp_img
+    ImageDraw.Draw(temp_img).text((1, -1), n, fill=(211, 220, 220), font=ui_font, stroke_fill=(0, 0, 0))
+    return temp_img
 
 
 if __name__ == '__main__':
-    script_path = Path(__file__).parent
-    font_bold_path = (script_path / '../data/Jovanny Lemonad - Bender-Bold.otf').resolve()
+    ui_dir = (script_path / '../data/template/UI')
+    if not ui_dir.exists() or not ui_dir.is_dir():
+        ui_dir.mkdir()
 
-    ui_templates_to_build = [
-        "HEADWEAR",
-        "EARPIECE",
-        "EYEWEAR",
-        "ON SLING",
-        "ON BACK",
-        "ARMBAND",
-        "SHEATH",
-        "HOLSTER",
-        "FACE COVER",
-        "TACTICAL RIG",
-        "BACKPACK"
-    ]
+    with open(item_json_path) as item_json_file:
+        data = json.load(item_json_file)
+        slots = list(data.keys())
 
-    ui_font = ImageFont.truetype(font_bold_path.as_posix(), 20)
-    for name, image in map(lambda n: to_ui_template(n, ui_font), ui_templates_to_build):
-        image.save((script_path / '../data/template/ui' / name).resolve().as_posix() + '.png')
+        for slot in slots:
+            slot_dir = (script_path / '../data/template' / slot)
+            ui_temp = to_ui_template(slot)
+            if not slot_dir.exists() or not slot_dir.is_dir():
+                slot_dir.mkdir()
+            ui_temp.save((ui_dir / slot).resolve().as_posix() + '.png')
 
-    headwear_templates_to_build = [
-        '6B47',
-        'TC-2001',
-        'TC-2002',
-        'UNTAR',
-        'Tactical Sport',
-        'Momex',
-        'Condor',
-        'AK-105',
-        'Day Pack',
-    ]
-
-    item_font = ImageFont.truetype(font_bold_path.as_posix(), 17)
-    for name, image in map(lambda n: to_item_template(n, item_font), headwear_templates_to_build):
-        image.save((script_path / '../data/template/headwear' / name).resolve().as_posix() + '.png')
+            for item in data[slot]:
+                item_temp = to_item_template(item)
+                item_temp.save((slot_dir / item).resolve().as_posix() + '.png')
